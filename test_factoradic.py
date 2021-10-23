4cf604bad50956bb95d788e3b7717f55e7473329
Mr. Eli W Jones <eli.jones@gmail.com>
2021-10-22 21:31:58 -0700

    test_factoradic: formatting.

--- test_factoradic.py
+++ test_factoradic.py
@@ -5,8 +5,9 @@ from factoradic import number_to_deck, deck_to_number
 
 
 class TestFactoradic(unittest.TestCase):
+
     def test_factoradic(self):
-        number = random.randrange(2 ** 128)
+        number = random.randrange(2**128)
         deck = number_to_deck(number=number)
 
         self.assertEqual(number, deck_to_number(deck=deck))
